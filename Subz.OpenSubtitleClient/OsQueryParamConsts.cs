6d0cb418d5f15bffc54c1092a38fc85e7435ebd9
Anıl Sezer <msnanil@hotmail.com>
2025-05-16 19:32:32 +0300

    feat: Query params will be formed with consts

---
+++ Subz.OpenSubtitleClient/OsQueryParamConsts.cs
@@ -0,0 +1,8 @@
+﻿namespace Subz.OpenSubtitleClient;
+
+public static class OsQueryParamConsts
+{
+    public const string Username = "OsUsername";
+    public const string Password = "OsPassword";
+    public const string ApiKey = "OsApiKey";
+}
---
+++ Subz/Consts/ConfigurationQueryParamConsts.cs
@@ -0,0 +1,8 @@
+﻿namespace Subz.Consts;
+
+public static class ConfigurationQueryParamConsts
+{
+    public const string SelectedLanguages = "langs";
+    public const string HearingImpairedOption = "hi";
+    public const string DiscordWebhook = "dw";
+}
