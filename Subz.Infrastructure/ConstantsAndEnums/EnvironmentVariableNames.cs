47d76812e0fbc6d809171558d04eda519c38164a
Anıl Sezer <msnanil@hotmail.com>
2025-05-17 13:06:08 +0300

    feat: New env variable name: APP_USER_AGENT

--- Subz.Infrastructure/ConstantsAndEnums/EnvironmentVariableNames.cs
+++ Subz.Infrastructure/ConstantsAndEnums/EnvironmentVariableNames.cs
@@ -4,4 +4,5 @@ namespace Subz.Infrastructure.ConstantsAndEnums;
 public static class EnvironmentVariableNames
 {
     public const string DevOrProd = "ASPNETCORE_ENVIRONMENT";
+    public const string AppUserAgent = "APP_USER_AGENT";
 }
