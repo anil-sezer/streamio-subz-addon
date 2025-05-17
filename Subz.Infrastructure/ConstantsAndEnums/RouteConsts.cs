8bf79700a6c257cb8cd5161f01e749ebc0c75867
Anıl Sezer <msnanil@hotmail.com>
2025-05-17 13:03:31 +0300

    feat: RouteConsts will be used everywhere, they are a must on this

---
+++ Subz.Infrastructure/ConstantsAndEnums/RouteConsts.cs
@@ -0,0 +1,11 @@
+﻿namespace Subz.Infrastructure.ConstantsAndEnums;
+
+public static class RouteConsts
+{
+    public const string Api = "api";
+    public const string SubtitleSearchRoute = $"{Api}/subtitles";
+    public const string SubtitleServeRoute = Api+"/download-subtitle";
+    public const string Manifest = $"{Api}/manifest.json";
+    
+    public const string FileId = "fileId";
+}
