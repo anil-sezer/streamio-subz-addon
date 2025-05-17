2f11a85a863a98c721358515a7cc54456bfcd01a
Anıl Sezer <msnanil@hotmail.com>
2025-05-17 12:50:57 +0300

    refactor: Globals.cs files

---
+++ Subz.OpenSubtitleClient/Services/Dtos/Search/SearchRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace Subz.OpenSubtitleClient.Services.Dtos.Search;
+
+// todo: tmdb_id lazım olur mu?
+public class SearchRequest
+{
+    [JsonPropertyName("imdb_id")]
+    public required string ImdbId { get; init; }
+    
+    [JsonPropertyName("hearing_impaired")]
+    public required string HearingImpairedOption { get; init; }
+    
+    [JsonPropertyName("languages")]
+    public required List<string> Languages { get; init; }
+    
+    // For TV shows
+    [JsonPropertyName("season_number")]
+    public string? SeasonNumber { get; init; }
+    [JsonPropertyName("episode_number")]
+    public string? EpisodeNumber { get; init; }
+}
---
+++ Subz.OpenSubtitleClient/Services/Dtos/Search/SearchResponse.cs
@@ -0,0 +1,22 @@
+﻿using Subz.OpenSubtitleClient.Services.Dtos.Search.SubDtos;
+
+namespace Subz.OpenSubtitleClient.Services.Dtos.Search;
+
+// todo: tmdb_id lazım olur mu?
+public class SearchResponse: OsResponseBase
+{
+    [JsonPropertyName("total_pages")]
+    public required int total_pages { get; set; }
+    
+    [JsonPropertyName("total_count")]
+    public required int total_count { get; set; }
+    
+    [JsonPropertyName("per_page")]
+    public required int per_page { get; set; }
+    
+    [JsonPropertyName("page")]
+    public required int page { get; set; }
+    
+    [JsonPropertyName("data")]
+    public required List<Data> data { get; set; }
+}
